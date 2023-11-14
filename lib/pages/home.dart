bbf66350bf630f7adee49033887d4a1f8ff6f6a7
riskiaulia21 <riskiaulia518@gmail.com>
2023-11-14 10:43:58 +0700

    Laying the Foundations of the Journal App

---
+++ lib/pages/home.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  static const _methodChannel = const MethodChannel('platformchannel.companyname.com/deviceinfo');
+
+  // Get device info
+  String _deviceInfo = '';
+
+  Future<void> _getDeviceInfo() async {
+    String deviceInfo;
+    try {
+      deviceInfo = await _methodChannel.invokeMethod('getDeviceInfo');
+    } on PlatformException catch (e) {
+      deviceInfo = "Failed to get device info: '${e.message}'.";
+    }
+
+    setState(() {
+      _deviceInfo = deviceInfo;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getDeviceInfo();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Platform Channel'),
+      ),
+      body: FutureBuilder(
+        initialData: [],
+        future: _loadJournals(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          return !snapshot.hasData
+              ? Center(child: CircularProgressIndicator())
+              : _buildListViewSeparated(snapshot);
+        },
+      ),
+          bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        child: Padding(padding: const EdgeInsets.all(24.0)),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Add Journal Entry',
+        child: Icon(Icons.add),
+        onPressed: () async {
+          _addOrEditJournal(add: true, index: -1, journal: Journal());
+        },
+      ),
+    );
+  }
+  
+  //tambah sendiri
+  _loadJournals() {}
+  
+  _buildListViewSeparated(AsyncSnapshot snapshot) {}
+  
+  Journal() {}
+  
+  void _addOrEditJournal({required bool add, required int index, required journal}) {}
+}
